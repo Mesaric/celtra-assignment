45a5a55134eff6b4249fec8d28a3c1e37ea64488
Mesaric <niko.mesar@gmail.com>
2023-10-23 12:52:00 +0200

    Added index page js, css. Ad space now populated with images from 3rd party API. Finished basic index page for visualization

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-
- 
 const hostname = '127.0.0.1';
 const port = 3000;
  
 var express = require("express");
+var path = require('path');
 var app = express();
-
-
-app.listen(port, hostname, function(){
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(port, hostname, function() {
   console.log(`Application started and Listening on ${hostname}:${port}`);
 });
 
