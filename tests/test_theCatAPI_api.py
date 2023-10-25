d7b296f6aa5a762f0130f130a1b3617b35029c7a
Mesaric <niko.mesar@gmail.com>
2023-10-25 09:54:12 +0200

    Added tests for question 1 c

--- tests/test_theCatAPI_api.py
+++ tests/test_theCatAPI_api.py
@@ -1,6 +1,27 @@
 import requests
-ENDPOINT = "https://api.thecatapi.com/v1/images/search"
 
-def test_can_call_endpoint():
+def test_returnOneCat():
+    ENDPOINT = "https://api.thecatapi.com/v1/images/search"
+
     response = requests.get(ENDPOINT)
+
     assert response.status_code == 200
+    assert len(response.json()) == 1
+
+def test_returnTenCats():
+    ENDPOINT = "https://api.thecatapi.com/v1/images/search?limit=10"
+
+    response = requests.get(ENDPOINT)
+
+    assert response.status_code == 200
+    assert len(response.json()) == 10
+
+def test_returnCatById():
+    ENDPOINT = "https://api.thecatapi.com/v1/images"
+    IMAGE_ID = "c0r"
+
+    response = requests.get(ENDPOINT + "/" + IMAGE_ID)
+
+    assert response.status_code == 200
+    assert response.json()['id'] == IMAGE_ID
+    
